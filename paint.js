a6bfe106e6dc9df6ead82de2aa5d835fef912ab2
cindou <yeyelio@163.com>
2021-04-18 21:40:43 +0800

    canvas+触摸版完成 缺陷:不可快速滑动

--- paint.js
+++ paint.js
@@ -1,46 +1,66 @@
-const oDiv = document.querySelector(".box");
-let isPainting = false;
+const canvas = document.querySelector("canvas");
+// console.log(canvas);
+
+canvas.style.display = "block"; //类似于图片，转换为块状，防止高度上滑动
+// canvas.style.border = "1px solid red";
+
+canvas.width = document.documentElement.clientWidth;
+canvas.height = document.documentElement.clientHeight; //必须初始时就设置好宽高，防止后期缩放比例，造成边界羽化
+
+console.log(
+  document.documentElement.clientWidth,
+  document.documentElement.clientHeight
+);
+console.log(canvas.width, canvas.height);
+
+const context = canvas.getContext("2d");
+context.lineCap = "round";
+context.lineJoin = "round";
+context.strokeStyle = "red";
+context.lineWidth = "10"; //不需要带单位
+
+let preCoords; //上一次的坐标
 
 const isInTouch = "ontouchstart" in document.documentElement;
 // console.log(isInTouch);
 
-const creatDiv = (e) => {
-  console.log(e);
-  const newDiv = document.createElement("div");
-  newDiv.style.width = 10 + "px";
-  newDiv.style.height = 10 + "px";
-  newDiv.style.backgroundColor = "black";
-  newDiv.style.borderRadius = "50%";
-
-  newDiv.style.position = "absolute";
-  newDiv.style.left = e.clientX + "px";
-  newDiv.style.top = e.clientY + "px";
-  return newDiv;
+const drawLine = (coords, x, y) => {
+  context.beginPath();
+  context.moveTo(coords[0], coords[1]);
+  context.lineTo(x, y);
+  context.stroke();
 };
 
 if (isInTouch) {
-  oDiv.ontouchmove = (e) => {
+  canvas.ontouchstart = (e) => {
+    const p = e.touches[0];
+    // console.log(p.clientX, p.clientY);
+
+    preCoords = [p.clientX, p.clientY];
+  };
+
+  canvas.ontouchmove = (e) => {
     const p = e.touches[0];
     // console.log(p);
-
-    const newDiv = creatDiv(p);
-    oDiv.appendChild(newDiv);
+    drawLine(preCoords, p.clientX, p.clientY);
+    preCoords = [e.clientX, e.clientY];
   };
 } else {
-  oDiv.onmousedown = (e) => {
-    //   alert("hello");
+  let isPainting = false;
+
+  canvas.onmousedown = (e) => {
     isPainting = true;
+    preCoords = [e.clientX, e.clientY];
   };
 
-  oDiv.onmousemove = (e) => {
+  canvas.onmousemove = (e) => {
     if (isPainting) {
-      // alert("Hi");
-      const newDiv = creatDiv(e);
-      oDiv.appendChild(newDiv);
+      drawLine(preCoords, e.clientX, e.clientY);
+      preCoords = [e.clientX, e.clientY];
     }
   };
 
-  oDiv.onmouseup = () => {
+  canvas.onmouseup = () => {
     isPainting = false;
   };
 }
