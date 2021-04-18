c0d55f39a015f75cb7441aaeb96afff203045b9a
cindou <yeyelio@163.com>
2021-04-18 19:36:05 +0800

    添加了触摸屏检测 存在问题：div跨域渲染响应过慢，且极度浪费资源

--- paint.js
+++ paint.js
@@ -1,28 +1,46 @@
 const oDiv = document.querySelector(".box");
 let isPainting = false;
 
-oDiv.onmousedown = (e) => {
-  //   alert("hello");
-  isPainting = true;
+const isInTouch = "ontouchstart" in document.documentElement;
+// console.log(isInTouch);
+
+const creatDiv = (e) => {
+  console.log(e);
+  const newDiv = document.createElement("div");
+  newDiv.style.width = 10 + "px";
+  newDiv.style.height = 10 + "px";
+  newDiv.style.backgroundColor = "black";
+  newDiv.style.borderRadius = "50%";
+
+  newDiv.style.position = "absolute";
+  newDiv.style.left = e.clientX + "px";
+  newDiv.style.top = e.clientY + "px";
+  return newDiv;
 };
 
-oDiv.onmousemove = (e) => {
-  if (isPainting) {
-    // alert("Hi");
-    const newDiv = document.createElement("div");
-    newDiv.style.width = 10 + "px";
-    newDiv.style.height = 10 + "px";
-    newDiv.style.backgroundColor = "black";
-    newDiv.style.borderRadius = "50%";
+if (isInTouch) {
+  oDiv.ontouchmove = (e) => {
+    const p = e.touches[0];
+    // console.log(p);
 
-    newDiv.style.position = "absolute";
-    newDiv.style.left = e.clientX + "px";
-    newDiv.style.top = e.clientY + "px";
-
+    const newDiv = creatDiv(p);
     oDiv.appendChild(newDiv);
-  }
-};
+  };
+} else {
+  oDiv.onmousedown = (e) => {
+    //   alert("hello");
+    isPainting = true;
+  };
 
-oDiv.onmouseup = () => {
-  isPainting = false;
-};
+  oDiv.onmousemove = (e) => {
+    if (isPainting) {
+      // alert("Hi");
+      const newDiv = creatDiv(e);
+      oDiv.appendChild(newDiv);
+    }
+  };
+
+  oDiv.onmouseup = () => {
+    isPainting = false;
+  };
+}
